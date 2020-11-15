0a38f7f6924ff1a7a1956a598406649e866dd7ca
Mohammed Yasser <mohammedre4a@gmail.com>
2020-11-15 20:15:12 +0200

    refactor : create Read & write functions , Use lastIndexof instead of indexof()

--- src/LZ77.java
+++ src/LZ77.java
@@ -7,16 +7,16 @@ public class LZ77 {
     private ArrayList<String> Search = new ArrayList<>();
     private ArrayList<String> look_Ahead = new ArrayList<>();
     private ArrayList<Tags> tags = new ArrayList<Tags>();
-    private String OriginalPath = "C:/Users/mohammed/Desktop/Multimedia/LZ77/src/OriginalText.txt";
+    private String OriginalPath = "C:/Users/mohammed/Desktop/Multimedia/LZ77/src/alphabet.txt";
     private String CompressPath = "C:/Users/mohammed/Desktop/Multimedia/LZ77/src/CompressedText.txt";
     private String DecompressPath = "C:/Users/mohammed/Desktop/Multimedia/LZ77/src/DecompressedText.txt";
     String content = "";
-    int SearchSize = 8, look_AheadSize = 4;
+    int SearchSize = 1024, look_AheadSize =50;
 
     public void Compress() throws IOException {
+        tags.clear();
         Path path = Paths.get(OriginalPath);
-        content = Files.readString(path, StandardCharsets.US_ASCII);
-        content = content.replace("\n", "").replace("\r", "");// remove carrige return and line feed
+        content = ReadFromFile(path);
         int indexofText = 0;
         for (; indexofText < content.length() && indexofText < look_AheadSize; indexofText++)
             look_Ahead.add(String.valueOf(content.charAt(indexofText)));// Fill look ahead buffer
@@ -32,9 +32,9 @@ public class LZ77 {
                 if (indexofText < content.length()) {
                     look_Ahead.add(String.valueOf(content.charAt(indexofText++)));// Adding new character to the buffer
                 }
-            } else {// found matching
+            } else {// matching found
                 // pos=end-index of matching;
-                int pos = Search.size() - Search.indexOf(look_Ahead.get(0));
+                int pos = Search.size() - Search.lastIndexOf(look_Ahead.get(0));
                 // len= try get matching from search and count length
                 int len = 1; // default value
                 int iter = 1;
@@ -66,13 +66,12 @@ public class LZ77 {
             }
         }
         FileWriter out = new FileWriter(CompressPath);
-        for (int i = 0; i < tags.size(); i++)
-            out.write(tags.get(i).toString());
-        out.close();
-
+        ;
+        WriteToFile(out, tags);
     }
 
     public void Decompress() throws IOException {
+        tags.clear();
         File inputFile = new File(CompressPath);
         FileWriter outputFile = new FileWriter(DecompressPath);
         Scanner scan = new Scanner(inputFile);
@@ -84,24 +83,32 @@ public class LZ77 {
             int len = Integer.parseInt(arrofTags[1]);
             String nextChar = arrofTags[2];
             if (pos == 0 && len == 0)// first time appear in the original
-            {
-                outputFile.write(nextChar);
                 content += nextChar; // adding char to content
-            } else { // appeard once before at leasts
+            else { // appeard once before at leasts
                 String o = "";
                 int beginofSubString = content.length() - pos;
                 // there is position value , so go back p values to get char
-                o += content.charAt(beginofSubString);
+                //o += content.charAt(beginofSubString);
                 // take length substring and add it
-                o += content.substring(beginofSubString + 1, beginofSubString + len);
+                o += content.substring(beginofSubString, beginofSubString + len);
                 // add nextchar
                 o += nextChar;
-                // print in file
-                outputFile.write(o);
                 content += o;
             }
         }
+        outputFile.write(content);//print into file 
         scan.close();
         outputFile.close();
+    }
+
+    public void WriteToFile(FileWriter out, ArrayList<Tags> tags) throws IOException {
+        for (int i = 0; i < tags.size(); i++)
+            out.write(tags.get(i).toString());
+        out.close();
+    }
+
+    public String ReadFromFile(Path path) throws IOException {
+        return Files.readString(path, StandardCharsets.UTF_8).replace("\n", "").replace("\r", "");
+        // remove carrige return and line feed
     }
 }
